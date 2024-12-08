74a81f535c0fdc9fee093f1ebe521ec0c789e039
Alison-Cy <avc.cumbajin@yavirac.edu.ec>
2024-12-08 16:58:21 -0500

    trabajo con pantallas

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:navegar/src/pages/sing_in.dart';
+import 'package:navegar/src/pages/sing_up.dart';
+import 'package:navegar/src/widgets/icon_containers.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        body: Container(
+          width: double.infinity, //Se ajusta al tamaño
+          height: double.infinity,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: <Color>[
+            Color.fromARGB(255, 255, 149, 149),
+            Color.fromARGB(19, 255, 148, 129),
+          ], begin: Alignment.topCenter)),
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 200),
+            children: <Widget>[
+              //!Aqui se ubica el URL de una imagen
+              IconContainers(url: "images/logo.png"),
+              Text(
+                "Bienvenidos",
+                style: TextStyle(
+                  fontFamily: "PermanentMarKer",
+                  fontSize: 30.0,
+                ),
+              ),
+              Divider(
+                height: 30.0,
+              ),
+              SizedBox(
+                width: 60.0,
+                height: 60.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    final route =
+                        MaterialPageRoute(builder: (context) => SingUp());
+                    Navigator.push(context, route);
+                  },
+                  child: Text(
+                    "SING UP",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 92, 88, 88),
+                        fontFamily: "FredokaOne",
+                        fontSize: 30.0),
+                  ),
+                ),
+              ),
+              Divider(
+                height: 30.0,
+              ),
+              SizedBox(
+                width: 60.0,
+                height: 60.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    final route =
+                        MaterialPageRoute(builder: (context) => SingIn());
+                    Navigator.push(context, route);
+                  },
+                  child: Text(
+                    "SING IN",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 92, 88, 88),
+                        fontFamily: "FredokaOne",
+                        fontSize: 30.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
